080d5dbef01c0257f9aed2b8f4c15830537a66d5
vo0922 <vo0922@naver.com>
2021-12-30 21:18:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/firstSpringproject/FirstSpringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringprojectApplication.class, args);
+	}
+
+}
