3f128247eaec75f8b8f58389fda2204b09002b04
vo0922 <vo0922@naver.com>
2022-01-02 17:39:16 +0900

    CRUD finish

--- src/main/java/com/example/firstSpringproject/entity/Article.java
+++ src/main/java/com/example/firstSpringproject/entity/Article.java
@@ -5,12 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity // DB가 해당 객체를 인식 가능
+@Entity // DB가 해당 객체를 인식 가능 (해당 클래스로 테이블 만들기)
 @AllArgsConstructor
 @NoArgsConstructor // 디폴트 생성자
 @ToString
@@ -18,7 +15,7 @@ import javax.persistence.Id;
 public class Article {
 
     @Id // 대표값을 지정
-    @GeneratedValue // 1...2...3 자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id를 자동 생성 어노테이션!
     private Long id;
 
     @Column
