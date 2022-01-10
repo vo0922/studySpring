d534da09b2553feef0cc272dc7c9d59085139c9e
vo0922 <vo0922@naver.com>
2022-01-10 20:13:07 +0900

    comment service, controller, comment Rest API

---
+++ src/main/java/com/example/firstSpringproject/api/CommentApiController.java
@@ -0,0 +1,59 @@
+package com.example.firstSpringproject.api;
+
+import com.example.firstSpringproject.dto.CommentDto;
+import com.example.firstSpringproject.entity.Article;
+import com.example.firstSpringproject.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentApiController {
+    @Autowired
+    private CommentService commentService;
+
+    // 댓글 목록 조회
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        // 서비스에게 위임
+        List<CommentDto> dtos = commentService.comments(articleId);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
+    // 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에게 위임
+        CommentDto createdDto = commentService.create(articleId, dto);
+
+        // 응답
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
+
+    // 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에게 위임
+        CommentDto updateDto = commentService.update(id, dto);
+
+        // 응답
+        return ResponseEntity.status(HttpStatus.OK).body(updateDto);
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        CommentDto deleted = commentService.delete(id);
+
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(deleted):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+}
